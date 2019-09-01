bcc181778ca5422140c4e6276ba04ee120c39d61
Rajiv Nayan <46836551+Rajiv-Nayan@users.noreply.github.com>
2019-09-01 10:05:17 +0530

    Update credentials.py

--- credentials.py
+++ credentials.py
@@ -1,4 +1,4 @@
-account_sid='AC383810a378d46cf1539fd2126b2b323c'
-auth_token='eda68dc3257c8e673b074906eeba1a06'
-my_cell='+91 79 0337 4773'
-my_twilio='+1 205 606 5913'
+account_sid='Your account id of Twilio'
+auth_token='Your auth token of Twilio'
+my_cell='Your Cell number'
+my_twilio='Your Twilio number'
